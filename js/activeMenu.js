891599721d2a91829e85bc9c53e60741ff212dfc
beardofdoom <marcin.jusiak@gmail.com>
2019-03-17 18:07:01 +0100

    v2 of activeMenu.js script

--- js/activeMenu.js
+++ js/activeMenu.js
@@ -14,56 +14,27 @@ function activeMenuSection() {
 
 
     // Get nav__link from Desktop menu
-    const nav__link_home = document.querySelector('.nav__item:nth-child(1) .nav__link');
-    const nav__link_about = document.querySelector('.nav__item:nth-child(2) .nav__link');
-    const nav__link_blog = document.querySelector('.nav__item:nth-child(3) .nav__link');
-    const nav__link_portfolio = document.querySelector('.nav__item:nth-child(4) .nav__link');
-    const nav__link_contact = document.querySelector('.nav__item:nth-child(5) .nav__link');
+    const navLinks = [...document.querySelectorAll('.nav__link')];
 
     // Section from Top of the screen (in px)
-    const homeSectionFromTop = document.querySelector('.header').offsetTop;
-    const aboutSectionFromTop = document.querySelector('.services').offsetTop;
-    const blogSectionFromTop = document.querySelector('.blog').offsetTop;
-    const portfolioSectionFromTop = document.querySelector('.project').offsetTop;
-    const contactSectionFromTop = document.querySelector('.contact').offsetTop;
-    const companiesSectionFromTop = document.querySelector('.companies').offsetTop;
-
-    // Height of portfolio section
-    const portfolioSectionHeight = document.querySelector('.project').clientHeight;
-
-    // End of portfolio section from top of the screen (in px)
-    const portfolioSectionEndFromTop = portfolioSectionHeight + portfolioSectionFromTop;
+    const sectionsFromTop = [
+        document.querySelector('.header').offsetTop,
+        document.querySelector('.services').offsetTop,
+        document.querySelector('.blog').offsetTop,
+        document.querySelector('.project').offsetTop,
+        document.querySelector('.contact').offsetTop,
+        document.querySelector('.companies').offsetTop
+    ];
 
 
-    if (scrollValueWithNavbar > homeSectionFromTop && scrollValueWithNavbar < aboutSectionFromTop) {
-        nav__link_home.classList.add('active');
-    } else {
-        nav__link_home.classList.remove('active');
-    }
+    navLinks.forEach((navLink, i) => {
+        if (scrollValueWithNavbar > sectionsFromTop[i] && scrollValueWithNavbar < sectionsFromTop[i + 1]) {
+            navLinks[i].classList.add('active');
+        } else {
+            navLinks[i].classList.remove('active');
+        }
+    })
 
-    if (scrollValueWithNavbar > aboutSectionFromTop && scrollValueWithNavbar < blogSectionFromTop) {
-        nav__link_about.classList.add('active');
-    } else {
-        nav__link_about.classList.remove('active');
-    }
-
-    if (scrollValueWithNavbar > blogSectionFromTop && scrollValueWithNavbar < portfolioSectionFromTop) {
-        nav__link_blog.classList.add('active');
-    } else {
-        nav__link_blog.classList.remove('active');
-    }
-
-    if (scrollValueWithNavbar > portfolioSectionFromTop && scrollValueWithNavbar < portfolioSectionEndFromTop) {
-        nav__link_portfolio.classList.add('active');
-    } else {
-        nav__link_portfolio.classList.remove('active');
-    }
-
-    if (scrollValueWithNavbar > contactSectionFromTop && scrollValueWithNavbar < companiesSectionFromTop) {
-        nav__link_contact.classList.add('active');
-    } else {
-        nav__link_contact.classList.remove('active');
-    }
 }
 
 window.addEventListener('scroll', activeMenuSection);
